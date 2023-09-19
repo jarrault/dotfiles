2aea6701af59c775e5db865b201ac293e5874ea7
Julien Arrault <56845840+jarrault@users.noreply.github.com>
2023-09-19 18:57:00 +0200

    Update gnome.sh

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -7,3 +7,6 @@ gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
 
 # Display seconds in the clock
 gsettings set org.gnome.desktop.interface clock-show-seconds true
+
+# Remove disks from the dock
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
